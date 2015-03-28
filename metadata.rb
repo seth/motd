b8991fd5e670771052064ef955d7e793981253db
Seth Falcon <seth@chef.io>
2015-03-28 13:06:01 -0700

    initial

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'motd'
+maintainer       'Chef Software, Inc'
+maintainer_email 'seth@chef.io'
+license          'apache2'
+description      'Installs/Configures motd'
+long_description 'Installs/Configures motd'
+version          '0.1.1'
+
