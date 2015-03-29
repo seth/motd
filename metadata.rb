600d42418a14745efed1f2934dfabe267bddd05b
Seth Falcon <seth@chef.io>
2015-03-28 19:03:57 -0700

    new message

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,4 @@ maintainer_email 'seth@chef.io'
 license          'apache2'
 description      'Installs/Configures motd'
 long_description 'Installs/Configures motd'
-version          '0.1.2'
-
+version          '0.1.3'
